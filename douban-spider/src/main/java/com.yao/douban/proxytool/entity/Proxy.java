f239b3174390a3b5c66ff89e4740283614d1b8db
DESKTOP-PCR84PE\单耀 <sqshanyao@163.com>
2018-01-27 23:19:47 +0800

    代理实体添加延迟时间

--- douban-spider/src/main/java/com.yao/douban/proxytool/entity/Proxy.java
+++ douban-spider/src/main/java/com.yao/douban/proxytool/entity/Proxy.java
@@ -1,16 +1,29 @@
 package com.yao.douban.proxytool.entity;
 
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by 单耀 on 2018/1/26.
  */
-public class Proxy {
+public class Proxy implements Delayed {
 
     private String ip;
 
     private int port;
 
+    private long lastSuccessTime;
     //来源
     private String dataSource;
+
+    private long timeIntervsl;//任务间隔时间
+
+    public Proxy(String ip, Integer port, long delayTime, String dataSource) {
+        this.ip = ip;
+        this.port = port;
+        this.timeIntervsl = TimeUnit.NANOSECONDS.convert(delayTime,TimeUnit.MILLISECONDS) + System.nanoTime();
+        this.dataSource = dataSource;
+    }
 
     public String getIp() {
         return ip;
@@ -34,5 +47,29 @@ public class Proxy {
 
     public void setDataSource(String dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public long getLastSuccessTime() {
+        return lastSuccessTime;
+    }
+
+    public void setLastSuccessTime(long lastSuccessTime) {
+        this.lastSuccessTime = lastSuccessTime;
+    }
+
+    public long getTimeIntervsl() {
+        return timeIntervsl;
+    }
+
+    public void setTimeIntervsl(long timeIntervsl) {
+        this.timeIntervsl = timeIntervsl;
+    }
+
+    public long getDelay(TimeUnit unit) {
+        return 0;
+    }
+
+    public int compareTo(Delayed o) {
+        return 0;
     }
 }
