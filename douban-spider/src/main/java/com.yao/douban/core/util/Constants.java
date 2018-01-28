5df0cac97bcbe3bcd94c6d0ec4403508a748d095
DESKTOP-PCR84PE\单耀 <sqshanyao@163.com>
2018-01-28 23:33:57 +0800

    开始url

--- douban-spider/src/main/java/com.yao/douban/core/util/Constants.java
+++ douban-spider/src/main/java/com.yao/douban/core/util/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
     public static int ConnectionTimeout = 5000;
 
     public static int TIMEOUT = 10000;
+
+    public static String STRTY_URL = "https://movie.douban.com/j/search_subjects?type=movie&tag=%E8%B1%86%E7%93%A3%E9%AB%98%E5%88%86&sort=rank&page_limit=20&page_start=0";
 }
