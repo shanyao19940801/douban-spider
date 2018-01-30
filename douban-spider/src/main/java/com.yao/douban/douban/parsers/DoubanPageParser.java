3fe6f6265a14a47b51d041ea1d291352016cd2dd
DESKTOP-PCR84PE\单耀 <sqshanyao@163.com>
2018-01-30 23:32:13 +0800

    豆瓣模块编写

--- douban-spider/src/main/java/com.yao/douban/douban/parsers/DoubanPageParser.java
+++ douban-spider/src/main/java/com.yao/douban/douban/parsers/DoubanPageParser.java
@@ -1,7 +1,15 @@
 package com.yao.douban.douban.parsers;
 
+import com.yao.douban.douban.entity.Move;
+
+import java.util.List;
+
+import static javafx.scene.input.KeyCode.T;
+
 /**
  * Created by 单耀 on 2018/1/30.
  */
 public interface DoubanPageParser {
+    List<Move> parser(String html);
+    List<T> parserTest(String html);
 }
---
+++ douban-spider/src/main/java/com.yao/douban/douban/parsers/move/MoveParser.java
@@ -0,0 +1,7 @@
+package com.yao.douban.douban.parsers.move;
+
+/**
+ * Created by 单耀 on 2018/1/30.
+ */
+public class MoveParser {
+}
