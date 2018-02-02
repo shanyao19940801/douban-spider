1c0e88a3228ab506ae192f0f647c7e1f8a79c9f4
DESKTOP-PCR84PE\单耀 <sqshanyao@163.com>
2018-02-02 23:27:36 +0800

    parser工厂类

--- douban-spider/src/main/java/com.yao/douban/douban/parsers/DoubanParserFactory.java
+++ douban-spider/src/main/java/com.yao/douban/douban/parsers/DoubanParserFactory.java
@@ -4,4 +4,15 @@ package com.yao.douban.douban.parsers;
  * Created by 单耀 on 2018/1/30.
  */
 public class DoubanParserFactory {
+    public static DoubanPageParser getDoubanParserFactory(Class clzz) {
+        try {
+            return (DoubanPageParser) clzz.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
 }
