d9939065ceac7a29cfc52b8dd99095b9695e5ccd
DESKTOP-PCR84PE\单耀 <sqshanyao@163.com>
2018-02-02 23:28:44 +0800

    通过泛型创建通用接口

--- douban-spider/src/main/java/com.yao/douban/douban/parsers/DoubanPageParser.java
+++ douban-spider/src/main/java/com.yao/douban/douban/parsers/DoubanPageParser.java
@@ -1,15 +1,11 @@
 package com.yao.douban.douban.parsers;
 
-import com.yao.douban.douban.entity.Move;
-
 import java.util.List;
-
-import static javafx.scene.input.KeyCode.T;
 
 /**
  * Created by 单耀 on 2018/1/30.
  */
-public interface DoubanPageParser {
-    List<Move> parser(String html);
-    List<T> parserTest(String html);
+public interface DoubanPageParser<T> {
+//    public List<Move> parser(String html);
+    public List<T> parser(String html);
 }
--- douban-spider/src/main/java/com.yao/douban/douban/parsers/move/MoveParser.java
+++ douban-spider/src/main/java/com.yao/douban/douban/parsers/move/MoveParser.java
@@ -1,7 +1,21 @@
 package com.yao.douban.douban.parsers.move;
 
+import com.yao.douban.douban.entity.move.Move;
+import com.yao.douban.douban.parsers.DoubanPageParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by 单耀 on 2018/1/30.
  */
-public class MoveParser {
+public class MoveParser implements DoubanPageParser<Move>{
+    public  List<Move> parser(String html) {
+        List<Move> list = new ArrayList<Move>();
+        Move move= new Move();
+        move.setId("1");
+        move.setName("test");
+        list.add(move);
+        return list;
+    }
 }
